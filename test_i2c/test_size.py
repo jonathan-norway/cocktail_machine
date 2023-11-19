5a00aa1a2afd0b736653583371d54bafe659abb1
jonathan-norway <jonakarl00@gmail.com>
2023-11-19 10:11:32 +0100

    f

---
+++ test_i2c/test_size.py
@@ -0,0 +1,14 @@
+from PyQt6.QtWidgets import QApplication
+
+app = QApplication([])
+
+# Get the primary screen
+primary_screen = app.primaryScreen()
+
+# Get the geometry of the primary screen
+screen_geometry = primary_screen.geometry()
+
+# Print the width and height of the screen
+print(f"Screen Size: {screen_geometry.width()}x{screen_geometry.height()} pixels")
+
+app.exec()
